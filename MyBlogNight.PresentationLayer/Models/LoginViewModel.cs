098027a66de1dee2b2edb7e46257d76be22ecd00
İsmail Baran <ismailbaran04@gmail.com>
2024-11-07 20:49:50 +0300

    login için özel sınıf oluşturuldu

---
+++ MyBlogNight.PresentationLayer/Models/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBlogNight.PresentationLayer.Models
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
