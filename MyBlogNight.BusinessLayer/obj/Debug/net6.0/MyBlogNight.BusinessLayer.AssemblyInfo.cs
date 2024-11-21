83f3186b440f81184f9577cda26e868c3c28b1af
İsmail Baran <ismailbaran04@gmail.com>
2024-11-21 21:58:52 +0300

    viewslar açıldı componentler yapıldı ve otomatik güncellemeler işlendi

--- MyBlogNight.BusinessLayer/obj/Debug/net6.0/MyBlogNight.BusinessLayer.AssemblyInfo.cs
+++ MyBlogNight.BusinessLayer/obj/Debug/net6.0/MyBlogNight.BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlogNight.BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65598421a1803351661840daea6bab783d563c29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214a2a19a9ef92b3acba69ee629517313d0722e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlogNight.BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlogNight.BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MyBlogNight.PresentationLayer/obj/Debug/net6.0/MyBlogNight.PresentationLayer.AssemblyInfo.cs
+++ MyBlogNight.PresentationLayer/obj/Debug/net6.0/MyBlogNight.PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlogNight.PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65598421a1803351661840daea6bab783d563c29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50389bb1935b107500afc352160c5cf224c53069")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlogNight.PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlogNight.PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
