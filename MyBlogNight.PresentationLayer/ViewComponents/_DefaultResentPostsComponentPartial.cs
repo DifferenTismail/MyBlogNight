4c938520b597ad107f0f2417a5e02be761267bf7
İsmail Baran <ismailbaran04@gmail.com>
2024-11-30 20:06:06 +0300

    yeni compoenntler açıdı

---
+++ MyBlogNight.PresentationLayer/ViewComponents/_DefaultResentPostsComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlogNight.BusinessLayer.Abstract;
+
+namespace MyBlogNight.PresentationLayer.ViewComponents
+{
+    public class _DefaultResentPostsComponentPartial : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+
+        public _DefaultResentPostsComponentPartial(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var resentPosts = _articleService.TResentPosts();
+            return View(resentPosts);
+        }
+    }
+}
