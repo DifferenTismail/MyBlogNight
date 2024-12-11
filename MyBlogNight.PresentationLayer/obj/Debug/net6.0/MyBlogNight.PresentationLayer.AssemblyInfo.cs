3a4ca667d357e27456eae7fb94fadf0ed193ca1a
İsmail Baran <ismailbaran04@gmail.com>
2024-12-11 23:41:13 +0300

    otomatik güncellemeler işlendi

--- MyBlogNight.PresentationLayer/obj/Debug/net6.0/MyBlogNight.PresentationLayer.AssemblyInfo.cs
+++ MyBlogNight.PresentationLayer/obj/Debug/net6.0/MyBlogNight.PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlogNight.PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a9d72bd329f0272d187e4b8433ccd800b06807a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea87726f80d74be8e15788536ef79b175460df28")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlogNight.PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlogNight.PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
