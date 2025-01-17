29771e9ba1629249c141105abeef1cdb4dad75bd
İsmail Baran <ismailbaran04@gmail.com>
2025-01-17 18:01:26 +0300

    otomatik güncellemeler tamamlandı

--- MyBlogNight.DtoLayer/obj/Debug/net6.0/MyBlogNight.DtoLayer.AssemblyInfo.cs
+++ MyBlogNight.DtoLayer/obj/Debug/net6.0/MyBlogNight.DtoLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlogNight.DtoLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c497f50c974ace10138e6bcbc08e9468152cb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a4ca667d357e27456eae7fb94fadf0ed193ca1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlogNight.DtoLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlogNight.DtoLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
